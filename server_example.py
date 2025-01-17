290885ede41d60f79a78744eaa62d5842604b330
WyvurnYT <153701920+WyvurnYT@users.noreply.github.com>
2025-01-17 11:12:58 -0800

    Update server_example.py

--- server_example.py
+++ server_example.py
@@ -89,6 +89,4 @@ if __name__ == "__main__":
     # server.enable_ssl(certfile="cert.pem", keyfile="privkey.pem")
     
     # Start the server
-    if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000))
-    server.run(host='0.0.0.0', port=port)
+    server.run(host='0.0.0.0', port=3000)
