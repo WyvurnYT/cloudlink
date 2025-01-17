4579e8f7cbac639bc88ef2e17cb274251b37606c
WyvurnYT <153701920+WyvurnYT@users.noreply.github.com>
2025-01-17 11:33:47 -0800

    Update server_example.py

--- server_example.py
+++ server_example.py
@@ -90,5 +90,5 @@ if __name__ == "__main__":
     
     # Start the server
     if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000))
+        port = int(os.environ.get("PORT", 3000))
     server.run(ip="0.0.0.0", port=port)
